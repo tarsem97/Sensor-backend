8d1e07f94dbaec14bed86e6dfe77bccdfc4537c4
tarsem <tarsemkohli97@gmail.com>
2025-08-09 22:14:14 +0530

    add code

--- sensordetail/settings.py
+++ sensordetail/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=^2ecc(q0q%$@1wa&*^%il=hi8&uqqtxv@kroy!8dtmg#k=dz-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
